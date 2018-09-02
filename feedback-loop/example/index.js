947bdabd36b6d5f4b20776cbcd68a573fe828ac2
Kayvon Falasiri <kayvonfalasiri@Kayvons-MacBook-Pro.local>
2018-09-02 18:57:41 -0400

    Attempting to get redis working via post and get methods in index.js and template.yaml

--- feedback-loop/example/index.js
+++ feedback-loop/example/index.js
@@ -1,13 +1,51 @@
 'use strict';
 
 // A simple hello world Lambda function
+// exports.handler = (event, context, callback) => {
+
+//     console.log('LOG: Received an event' );
+
+//     callback(null, {
+//         statusCode: 200,
+//         body: "Hello, World!"
+//     })
+
+// }
+
+
+
+//redis code
+
+// var redis = require("redis"),
+//     client = redis.createClient();
+
+// client.set("string key", "string val", redis.print);
+// client.hset("hash key", "hashtest 1", "some value", redis.print);
+
 exports.handler = (event, context, callback) => {
 
-    console.log('LOG: Received an event' );
+    let id =  (event.pathParameters || {}).product || false;
+    switch(event.httpMethod){
 
-    callback(null, {
-        statusCode: 200,
-        body: "Hello, World!"
-    })
+        case "GET":
+
+            if(id) {
+                callback(null, {body: "This is a READ operation on ID " + id});
+                return;  
+            } 
+
+            callback(null, {body: "This is a LIST operation, return all items"});
+            break;
+
+        case "POST":            
+            callback(null, {body: "This is a CREATE operation"}); 
+            break;
+
+        default:
+            // Send HTTP 501: Not Implemented
+            console.log("Error: unsupported HTTP method (" + event.httpMethod + ")");
+            callback(null, { statusCode: 501 })
+
+    }
 
 }

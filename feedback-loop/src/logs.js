916d22bda438137918270c400d3837cca9c01317
kf820 <kayvonf820@gmail.com>
2018-09-24 21:07:07 -0400

    kinesis initial commit

--- feedback-loop/src/logs.js
+++ feedback-loop/src/logs.js
@@ -1,29 +1,39 @@
 'use strict'
 
-module.exports.respond = function (event, callback) {
-  const redis = require('./redis')()
+module.exports.respond = function (event, context, callback) {
 
-  let error, response
-  let key = 'counter'
-  redis.on('end', () => {
-    callback(error, response)
-  })
+  event.Records.forEach((record) => {
+        // Kinesis data is base64 encoded so decode here
+        const payload = new Buffer(record.kinesis.data, 'base64').toString('ascii');
+        console.log('Decoded payload:', payload);
+    });
+    callback(null, `Successfully processed ${event.Records.length} records.`);
 
-  redis.on('ready', function () {
-      // set counter = 0
-    redis.set(key, 0, (err) => {
-        if (err) {
-          redis.quit(() => {
-            error = err
-          })
-        } else {
-          redis.incr(key, res, (err) => {
-            redis.quit(() => {
-                response = res
-              })
-          })
-        }
-      })
 
-  })
+  // const redis = require('./redis')()
+
+  // let error, response
+  // let key = 'counter'
+  // redis.on('end', () => {
+  //   callback(error, response)
+  // })
+
+  // redis.on('ready', function () {
+  //     // set counter = 0
+  //   redis.set(key, 0, (err) => {
+  //       if (err) {
+  //         redis.quit(() => {
+  //           error = err
+  //         })
+  //       } else {
+  //         redis.incr(key, res, (err) => {
+  //           redis.quit(() => {
+  //               response = res
+  //             })
+  //         })
+  //       }
+  //     })
+
+  // })
 }
+
